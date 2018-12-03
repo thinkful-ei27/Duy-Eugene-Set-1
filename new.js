6fdbf20dd54a063ee64a8b630434699ce961ea8f
Duy Le <duyvietle13@gmail.com>
2018-12-03 13:44:33 -0800

    added errors

--- new.js
+++ new.js
@@ -1,11 +1,29 @@
+'use strict';
 function yearOfBirth(age){
-return 2016 - age;
-};
+    if(age < 0){
+        throw new Error("Age can not be negative");
+    }
+  return 2016 - age;
+}
+
+console.log(yearOfBirth(25));
 
 function createGreeting(name, age) {
-    const yearOfBirth = 2016 -age;
-    return `I was born in ${yearOfBirth}`;
-};
-
-const greeting1 = createGreeting();
+  if(typeof age !== "number"){
+      throw new TypeError("Not a Number");
+  }
+  else if(typeof name !== 'string'){
+      throw new TypeError("Not a String");
+  }
+  else{let year = yearOfBirth(age);
+  return `Hi my name is ${name} and I was born in ${year}`;
+ }
+}
+try {
+const greeting1 = createGreeting('John', 29);
 console.log(greeting1);
+} 
+catch (e){
+    console.log("NOOO");
+    console.log(e);
+}
