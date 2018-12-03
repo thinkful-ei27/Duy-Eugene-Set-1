4087e922e03355e824c08e31285ae1920cba2fd6
Duy Le <duyvietle13@gmail.com>
2018-12-03 12:13:48 -0800

    edited the function

--- new.js
+++ new.js
@@ -1,6 +1,7 @@
 
 function createGreeting {
-    console.log('Hi, my names is Duy and I'm 29 years old');
+    return 'Hi, my names is Duy and I'm 29 years old';
 };
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
